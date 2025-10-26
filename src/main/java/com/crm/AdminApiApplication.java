4ef5da471247642085945a8b26eb8ab8c806074e
qqs <952666721@qq.com>
2025-10-26 16:55:48 +0800

    feat: 商品管理

--- src/main/java/com/crm/AdminApiApplication.java
+++ src/main/java/com/crm/AdminApiApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AdminApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -17,5 +19,4 @@ public class AdminApiApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(AdminApiApplication.class);
     }
-
 }
